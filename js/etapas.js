9c4fda74ca309692df4ca1206cd72919e8e34039
CRISTIANO DA SILVA FERREIRA <68359459+CristianoDaSilvaFerreira@users.noreply.github.com>
2021-06-09 18:19:57 -0300

    :up: finalizado projeto

---
+++ js/etapas.js
@@ -0,0 +1,87 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numero: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Fulando de Tal',
+                partido: 'ABC',
+                fotos: [
+                    {
+                        url: 'img/1.jpg',
+                        legenda: 'Vereador',
+                        small: false
+                    }
+                ]
+            },
+
+            {
+                numero: '77222',
+                nome: 'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos: [
+                    {
+                        url: 'img/2.jpg',
+                        legenda: 'Vereador'
+                    }
+                ]
+            },
+
+        ]
+    },
+
+    {
+        titulo: 'PREFEITO',
+        numero: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos: [
+                    {
+                        url: 'img/3.jpg',
+                        legenda: 'Prefeito',
+                    },
+                    {
+                        url: 'img/4.jpg',
+                        legenda: 'Vice-Prefeito',
+                        small: true,
+                    },
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Zulano',
+                partido: 'QWERTY',
+                vice: 'Zul',
+                fotos: [
+                    {
+                        url: 'img/1.jpg',
+                        legenda: 'Prefeito',
+                    },
+                    {
+                        url: 'img/2.jpg',
+                        legenda: 'Vice-Prefeito',
+                        small: true,
+                    },
+                ]
+            },
+
+            {
+                numero: 77222,
+                nome: 'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos: [
+                    {
+                        url: 'img/2.jpg',
+                        legenda: 'Vereador'
+                    }
+                ]
+            },
+
+        ]
+    },  
+]
